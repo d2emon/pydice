30cb4f9adc690475293160c3a366be965018d4a7
Dmitry Kutsenko <d2emonium@gmail.com>
2018-10-08 17:20:42 +0300

    Add multiplier

--- dice/dice.py
+++ dice/dice.py
@@ -2,19 +2,20 @@ import random
 import logging
 
 
-def d(count=1, max_value=6, min_value=1, modifier=0, drop_min=0, drop_max=0):
+def d(count=1, max_value=6, min_value=1, modifier=0, multiplier=1, drop_min=0, drop_max=0):
     dice = Dice(count, max_value, min_value, modifier)
     roll = list(dice.roll())
     roll.sort()
-    return sum(roll[drop_min:drop_max])
+    return sum(roll[drop_min:drop_max]) * multiplier
 
 
 class Dice:
-    def __init__(self, count=1, max_value=6, min_value=1, modifier=0):
+    def __init__(self, count=1, max_value=6, min_value=1, modifier=0, multiplier=1):
         self.count = count
         self.min_value = min_value
         self.max_value = max_value
         self.modifier = modifier
+        self.multiplier = multiplier
 
         self.value = None
 
@@ -28,7 +29,7 @@ class Dice:
 
     def roll(self):
         for _ in range(self.count):
-            self.value = random.randint(self.min_value, self.max_value) + self.modifier
+            self.value = random.randint(self.min_value, self.max_value) * self.multiplier + self.modifier
             logging.debug("%s = %d", self.name, self.value)
             yield self.value
 
