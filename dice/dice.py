dd031d68ea500e99b568d361c271ca89490690ff
d2emon <d2emonium@gmail.com>
2020-09-26 15:45:35 +0300

    Fix bug with modifier

--- dice/dice.py
+++ dice/dice.py
@@ -10,28 +10,85 @@ def d(count=1, max_value=6, min_value=1, modifier=0, multiplier=1, drop_min=0, d
 
 
 class Dice:
-    def __init__(self, count=1, max_value=6, min_value=1, modifier=0, multiplier=1):
+    def __init__(
+        self,
+        count=1,
+        max_value=6,
+        min_value=1,
+        modifier=0,
+        multiplier=1,
+        drop_min=0,
+        drop_max=0,
+        # throws=1,
+        # characteristics=6,
+        # group=1,
+        # adjust=0.0,
+        # points=0,
+        # average_points=0,
+        # keep_best=False,
+        # max_characteristic=18,
+    ):
+        """
+        Rolling dice
+        :param count: number of dices
+        :param max_value: max value of roll
+        :param min_value: min value of roll
+        :param modifier: roll modifier
+        :param multiplier: roll multiplier
+        :param drop_min: drop minimal dices
+        :param drop_max: drop maximal dices
+        """
         self.count = count
         self.min_value = min_value
         self.max_value = max_value
         self.modifier = modifier
         self.multiplier = multiplier
+        self.drop_min = drop_min
+        self.drop_max = drop_max
 
         self.value = None
 
     @property
     def name(self):
-        return "{}d{} + {}".format(
-            self.count,
-            self.max_value,
-            self.modifier
-        )
+        """
+        Dice name
+        :return:
+        """
+        name = f"{self.count}d{self.max_value}"
+        if self.modifier > 0:
+            name = f"{name} + {self.modifier}"
+        if self.multiplier > 1:
+            name = f"({name}) * {self.multiplier}"
+        return name
 
     def roll(self):
+        """
+        Roll next dice
+        :return: roll result
+        """
         for _ in range(self.count):
-            self.value = random.randint(self.min_value, self.max_value) * self.multiplier + self.modifier
+            self.value = random.randint(self.min_value, self.max_value)
             logging.debug("%s = %d", self.name, self.value)
             yield self.value
+
+    def total(self):
+        """
+        Roll result
+        :return:
+        """
+        rolls = list(self.roll())
+
+        # Drop rolls
+        rolls.sort()
+        if self.drop_min:
+            rolls = rolls[self.drop_min:]
+        if self.drop_max:
+            rolls = rolls[:-self.drop_max]
+
+        # Apply modifier & multiplier
+        self.value = (sum(rolls) + self.modifier) * self.multiplier
+        logging.debug("%s = %d", self.name, self.value)
+        return self.value
 
     def __str__(self):
         return "{} = {}".format(self.name, self.value)
@@ -44,3 +101,18 @@ class Dice:
 
     def __gt__(self, other):
         return self.value > other.value
+
+
+class RollMethod:
+    def __init__(
+        self,
+        method_id,
+        name,
+        *args,
+        method_type=None,
+        **kwargs,
+    ):
+        self.method_id = method_id
+        self.name = name
+        self.method_type = method_type
+        self.dice = Dice(*args, **kwargs)
